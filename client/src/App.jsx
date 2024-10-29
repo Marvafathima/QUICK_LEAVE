f7ba31732a50efd16466a3f627e3e368ecaefc0e
Marvafathima <marvafathima62@gmail.com>
2024-10-29 17:44:14 +0530

    configured material tailwind to my application

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@material-tailwind/react";
+ 
+
+
+function App() {
+
+
+  return (
+    <>
+     <Button variant="filled">Hellooo</Button>
+    </>
+  )
+}
+
+export default App
