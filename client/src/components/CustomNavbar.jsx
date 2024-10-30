02fe4a7ab3720ed51475c5bfba3a7d73e1317360
Marvafathima <marvafathima62@gmail.com>
2024-10-31 01:18:31 +0530

    navbar for two users done

--- client/src/components/CustomNavbar.jsx
+++ client/src/components/CustomNavbar.jsx
@@ -20,7 +20,7 @@ export const CustomNavbar = () => {
     const isAuthenticated = useSelector(selectIsAuthenticated);
     const user = useSelector(selectCurrentUser);
     const [openNav, setOpenNav] =useState(false);
-
+    const userRole = useSelector((state) => state.auth.user?.role);
 
     useEffect(() => {
       window.addEventListener(
@@ -36,7 +36,11 @@ export const CustomNavbar = () => {
 
 
       const navList = (
+        
         <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+         
+          {userRole === "employee" ? (
+             <>
           <Typography
             as="li"
             variant="small"
@@ -44,7 +48,7 @@ export const CustomNavbar = () => {
             className="p-1 font-normal"
           >
             <a href="#" className="flex items-center">
-              Pages
+              Dashboard
             </a>
           </Typography>
           <Typography
@@ -54,7 +58,7 @@ export const CustomNavbar = () => {
             className="p-1 font-normal"
           >
             <a href="#" className="flex items-center">
-              Account
+              Apply for leave
             </a>
           </Typography>
           <Typography
@@ -64,7 +68,7 @@ export const CustomNavbar = () => {
             className="p-1 font-normal"
           >
             <a href="#" className="flex items-center">
-              Blocks
+            My Leave Requests
             </a>
           </Typography>
           <Typography
@@ -74,103 +78,58 @@ export const CustomNavbar = () => {
             className="p-1 font-normal"
           >
             <a href="#" className="flex items-center">
-              Docs
+            Total leaves Report:
             </a>
           </Typography>
+          </>
+          ) : (
+            // Render manager options if the user role is manager
+            <>
+              <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+              >
+                <a href="#" className="flex items-center">
+                  Dashboard
+                </a>
+              </Typography>
+              <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+              >
+                <a href="#" className="flex items-center">
+                  Leave Requests
+                </a>
+              </Typography>
+              <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+              >
+                <a href="#" className="flex items-center">
+                  Rejected Requests
+                </a>
+              </Typography>
+              <Typography
+                as="li"
+                variant="small"
+                color="blue-gray"
+                className="p-1 font-normal"
+              >
+                <a href="#" className="flex items-center">
+                  Approved Requests
+                </a>
+              </Typography>
+            </>
+          )}
         </ul>
       );
-     
-//   return (
-//     <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll bg-gradient-to-b from-white to-green-300">
-//       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
-//         <div className="flex items-center justify-between text-blue-gray-900">
-//           <Typography
-//             as="a"
-//             href="#"
-//             className="mr-4 cursor-pointer py-1.5 font-bold text-2xl"
-//           >
-//             QUICKLEAVE
-//           </Typography>
-//           <div className="flex items-center gap-4">
-//             {/* <div className="mr-4 hidden lg:block">{navList}</div> */}
-//             {isAuthenticated && <div className="mr-4 hidden lg:block">{navList}</div>}
-//             <div className="flex items-center gap-x-1">
-
-//             <a href="/login">
-//                <Button
-//                 variant="text"
-//                 size="sm"
-//                 className="hidden lg:inline-block "
-               
-//               >
-//                 <span>Log In</span>
-//               </Button></a>
-//               <a href="/signup">
-//               <Button
-//                 variant="gradient"
-//                 size="sm"
-//                 className="hidden lg:inline-block"
-//                 color="green"
-//               >
-//                 <span>Sign up</span>
-//               </Button></a>
-//             </div>
-//             <IconButton
-//               variant="text"
-//               className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-//               ripple={false}
-//               onClick={() => setOpenNav(!openNav)}
-//             >
-//               {openNav ? (
-//                 <svg
-//                   xmlns="http://www.w3.org/2000/svg"
-//                   fill="none"
-//                   className="h-6 w-6"
-//                   viewBox="0 0 24 24"
-//                   stroke="currentColor"
-//                   strokeWidth={2}
-//                 >
-//                   <path
-//                     strokeLinecap="round"
-//                     strokeLinejoin="round"
-//                     d="M6 18L18 6M6 6l12 12"
-//                   />
-//                 </svg>
-//               ) : (
-//                 <svg
-//                   xmlns="http://www.w3.org/2000/svg"
-//                   className="h-6 w-6"
-//                   fill="none"
-//                   stroke="currentColor"
-//                   strokeWidth={2}
-//                 >
-//                   <path
-//                     strokeLinecap="round"
-//                     strokeLinejoin="round"
-//                     d="M4 6h16M4 12h16M4 18h16"
-//                   />
-//                 </svg>
-//               )}
-//             </IconButton>
-//           </div>
-//         </div>
-//         <MobileNav open={openNav}>
-//           {navList}
-//           <div className="flex items-center gap-x-1">
-//             <a href="/login">
-//             <Button ripple={true} fullWidth variant="text" size="sm" className="">
-//               <span>Log In</span>
-//             </Button ></a>
-//             <a href="/signup"><Button ripple={true} fullWidth variant="gradient" size="sm" className="">
-//               <span>Sign up</span>
-//             </Button></a>
-//           </div>
-//         </MobileNav>
-//       </Navbar>
-    
-//     </div>
-//   )
-// }
+  
 const authButtons = isAuthenticated ? (
   // Logged in state
   <div className="flex items-center gap-x-1">
