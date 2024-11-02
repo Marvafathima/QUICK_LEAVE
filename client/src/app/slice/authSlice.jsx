1e3f28c1dae520e24c44cfa9c7e3c4f783698580
Marvafathima <marvafathima62@gmail.com>
2024-11-03 00:43:51 +0530

    commit before solving the authorization error

--- client/src/app/slice/authSlice.jsx
+++ client/src/app/slice/authSlice.jsx
@@ -1,35 +1,4 @@
-// import { createSlice } from '@reduxjs/toolkit';
 
-// const initialState = {
-//   accessToken: null,
-//   refreshToken: null,
-//   role: null,
-//   isAuthenticated: false,
-// };
-
-// const authSlice = createSlice({
-//   name: 'auth',
-//   initialState,
-//   reducers: {
-//     setCredentials: (state, action) => {
-//       const { accessToken, refreshToken, role } = action.payload;
-//       state.accessToken = accessToken;
-//       state.refreshToken = refreshToken;
-//       state.role = role;
-//       state.isAuthenticated = true;
-//     },
-//     logout: (state) => {
-//       state.accessToken = null;
-//       state.refreshToken = null;
-//       state.role = null;
-//       state.isAuthenticated = false;
-//     },
-//   },
-// });
-
-// export const { setCredentials, logout } = authSlice.actions;
-// export default authSlice.reducer;
-// authSlice.js
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
