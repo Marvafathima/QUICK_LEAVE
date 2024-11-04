93a92f0b438d1ec9850604749fefa0c52ea8c94c
Marvafathima <marvafathima62@gmail.com>
2024-11-04 11:35:49 +0530

    app.css changed to remove width constrain,layut to old layout compoennt

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,31 +1,13 @@
-// import { CustomNavbar } from "./CustomNavbar";
-// import { CustomFooter } from "./CustomFooter";
-
-// const Layout = ({ children }) => {
-//   return (
-   
-//     <div className="flex min-h-screen flex-col">
-//       <CustomNavbar />
-//       <main className="flex-grow bg-gradient-to-b from-white to-green-300 px-4 py-12">
-//         <div className="mx-auto max-w-screen-md">{children}</div>
-//       </main>
-//       <CustomFooter />
-//     </div>
-//   );
-// };
-
-// export default Layout;
 import { CustomNavbar } from "./CustomNavbar";
 import { CustomFooter } from "./CustomFooter";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex min-h-screen flex-col overflow-x-hidden">
+   
+    <div className="flex min-h-screen flex-col">
       <CustomNavbar />
-      <main className="flex-grow bg-gradient-to-b from-white to-green-300 w-full">
-        <div className="mx-auto max-w-screen-md px-4 py-12">
-          {children}
-        </div>
+      <main className="flex-grow bg-gradient-to-b from-white to-green-300 px-4 py-12">
+        <div className="mx-auto max-w-screen-md">{children}</div>
       </main>
       <CustomFooter />
     </div>
@@ -33,3 +15,21 @@ const Layout = ({ children }) => {
 };
 
 export default Layout;
+// import { CustomNavbar } from "./CustomNavbar";
+// import { CustomFooter } from "./CustomFooter";
+
+// const Layout = ({ children }) => {
+//   return (
+//     <div className="flex min-h-screen flex-col overflow-x-hidden">
+//       <CustomNavbar />
+//       <main className="flex-grow bg-gradient-to-b from-white to-green-300 w-full">
+//         <div className="mx-auto max-w-screen-md px-4 py-12">
+//           {children}
+//         </div>
+//       </main>
+//       <CustomFooter />
+//     </div>
+//   );
+// };
+
+// export default Layout;
