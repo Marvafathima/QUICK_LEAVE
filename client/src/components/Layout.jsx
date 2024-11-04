bd9edf140678e9da8b9162444e3824f59a57d31a
Marvafathima <marvafathima62@gmail.com>
2024-11-04 11:28:13 +0530

    layout compoenent changed to remove margins in py and px

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,13 +1,31 @@
+// import { CustomNavbar } from "./CustomNavbar";
+// import { CustomFooter } from "./CustomFooter";
+
+// const Layout = ({ children }) => {
+//   return (
+   
+//     <div className="flex min-h-screen flex-col">
+//       <CustomNavbar />
+//       <main className="flex-grow bg-gradient-to-b from-white to-green-300 px-4 py-12">
+//         <div className="mx-auto max-w-screen-md">{children}</div>
+//       </main>
+//       <CustomFooter />
+//     </div>
+//   );
+// };
+
+// export default Layout;
 import { CustomNavbar } from "./CustomNavbar";
 import { CustomFooter } from "./CustomFooter";
 
 const Layout = ({ children }) => {
   return (
-   
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col overflow-x-hidden">
       <CustomNavbar />
-      <main className="flex-grow bg-gradient-to-b from-white to-green-300 px-4 py-12">
-        <div className="mx-auto max-w-screen-md">{children}</div>
+      <main className="flex-grow bg-gradient-to-b from-white to-green-300 w-full">
+        <div className="mx-auto max-w-screen-md px-4 py-12">
+          {children}
+        </div>
       </main>
       <CustomFooter />
     </div>
